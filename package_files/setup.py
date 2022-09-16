082b1ab926c67218cab235f90411febf87dd8c87
miguelzph <miguelzph@gmail.com>
2022-09-16 20:12:39 -0300

    first commit

---
+++ package_files/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distri_probability_tests', # nome único
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['distri_probability_tests'], # pasta
+      author = 'Miguel',
+      author_email = 'name@gmail.com',
+      zip_safe=False) # não pode ser zipado
